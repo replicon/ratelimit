f86a75bf56262385079f367d47f8b5f0f6cd723c
Vivek Kumar Shaw <vivek.shaw@replicon.com>
2024-01-15 15:23:54 +0530

    fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -n ${REDIS_AUTH_SSM_PATH:-} ]; then
+if [ -n "${REDIS_AUTH_SSM_PATH:-}" ]; then
     export REDIS_AUTH=$(/usr/local/bin/aws ssm get-parameter --name ${REDIS_AUTH_SSM_PATH} --with-decryption --query 'Parameter.Value' --output text)
 fi
 nohup /sync_config.sh &
