6a1dbdbfe08a0dd6614d4ced8025bc6a39e6a171
Vivek Kumar Shaw <vivek.shaw@replicon.com>
2024-01-15 14:17:26 +0530

    check if envs contain the redis auth ssm path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-export REDIS_AUTH=$(/usr/local/bin/aws ssm get-parameter --name ${REDIS_AUTH_SSM_PATH} --with-decryption --query 'Parameter.Value' --output text)
-
+if [ -n ${REDIS_AUTH_SSM_PATH:-} ]; then
+    export REDIS_AUTH=$(/usr/local/bin/aws ssm get-parameter --name ${REDIS_AUTH_SSM_PATH} --with-decryption --query 'Parameter.Value' --output text)
+fi
 nohup /sync_config.sh &
 /bin/ratelimit
