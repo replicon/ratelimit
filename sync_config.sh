83fbcf5032da63269ad25e3a80d762d2f190b999
Mike Roest <mike.roest@replicon.com>
2024-06-03 14:12:57 -0600

    fix paths

--- sync_config.sh
+++ sync_config.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 mkdir -p /extra_metrics
-/usr/local/bin/python3 /metrics/main.py >/dev/null 2>&1 &
+/bin/python3 /metrics/main.py >/dev/null 2>&1 &
 
 cd /srv/runtime_data/current
 
 while true
 do
-  /usr/local/bin/aws s3 sync --delete ${RLS_S3_PATH} data/
+  /bin/aws s3 sync --delete ${RLS_S3_PATH} data/
 
-  [ $? -eq 0 ] && /usr/local/bin/python3 data/${RLS_SCRIPT_DIR}config-generator.py \
+  [ $? -eq 0 ] && /bin/python3 data/${RLS_SCRIPT_DIR}config-generator.py \
     --no-docker-container \
     --static data/${RLS_STATIC_DIR} \
     --blacklist data/${RLS_BLACKLIST_DIR} \
