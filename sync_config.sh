380b45d4fb5efcf0db9976d2c95a573032387b02
ashwin <ashwin.r@replicon.com>
2024-09-02 16:58:14 +0530

    update aws path

--- sync_config.sh
+++ sync_config.sh
@@ -2,7 +2,7 @@
 
 export AWS_USE_FIPS_ENDPOINT=${AWS_USE_FIPS_ENDPOINT}
 
-/bin/aws --version
+/usr/local/bin/aws --version
 
 mkdir -p /extra_metrics
 /bin/python3 /metrics/main.py >/dev/null 2>&1 &
@@ -11,7 +11,7 @@ cd /srv/runtime_data/current
 
 while true
 do
-  /bin/aws s3 sync --delete ${RLS_S3_PATH} data/
+  /usr/local/bin/aws s3 sync --delete ${RLS_S3_PATH} data/
 
   [ $? -eq 0 ] && /bin/python3 data/${RLS_SCRIPT_DIR}config-generator.py \
     --no-docker-container \
